0efe8359b6d00a8dc6bdaa886ee170a699dd082c
changhyun yi <chhyyi@gmail.com>
2026-01-21 18:24:25 +0900

    test set generation, test codes working now

--- tests/test.py
+++ tests/test.py
@@ -6,47 +6,54 @@ main features:
         * StanevSuperCon
         * XuDataset (see comparison.XuDataset for details)
 
-    $ python -m unittest test.py
+    $ uv run python -m unittest test
+    
 """
 
-import unittest
 import importlib.resources as resources
+import unittest
+from pathlib import Path
 
-import pandas as pd
 from sklearn.metrics import r2_score
 
 from hsdu.dataset import Dataset, D2TableDataset
 from hsdu.comparison import XuTestHEA, StanevSuperCon
 
-class Test(unittest.TestCase):
-    """Test core class methods"""
-
+class TestDataset(unittest.TestCase):
     def test_dataset(self):
         """in-house dataset test
         
-        load test version of in-house dataset
+        if full_dataset.csv exists in hsdu.data.tests, use that. except, use test_dataset.csv
         """
-        # load dataset
-        with resources.as_file(resources.files("hsdu.data.tests") /"test_dataset.csv") as path:
-            test_dataset_path = path
-        dataset = Dataset(test_dataset_path, config="default_forward.json")
+        test_dataset_path: Path=None
+        with resources.as_file(resources.files("hsdu.data.tests") /"full_dataset.csv") as path:
+            test_dataset_path = path if Path(path).is_file() else None
 
-        # load stanev's supercon dataset 
-        ss_dataset = StanevSuperCon()
-        ss_dataset.pymatgen_comps()
-
-        xu_test_set = XuTestHEA()
+        if test_dataset_path is None:
+            with resources.as_file(resources.files("hsdu.data.tests") /"test_dataset.csv") as path:
+                test_dataset_path = path
 
         # load as a D2TableDataset
         dataset = D2TableDataset(test_dataset_path, exception_col=None)
         dataset.pymatgen_comps()
+
+        # load dataset
+        dataset = Dataset(test_dataset_path, config="default.json")
+        
+    def test_comparison(self):
+        # load stanev's supercon dataset 
+        ss_dataset = StanevSuperCon()
+        ss_dataset.pymatgen_comps()
+
+        _ = XuTestHEA()
 
         #xu_val_r2score: test XuDataset reproduces the snapshot
         xu_dataset = XuTestHEA()
         r2score = r2_score(xu_dataset.df["Experimental_T_c(K)"], xu_dataset.df["Predicted_T_c(K)"])
         print(f"r2_score:{r2score}")
         self.assertAlmostEqual(r2score, 0.9246, places=4)
-    
-if __name__ == '__main__':
-    #Test().test_dataset() #it is convenience for debug..
-    unittest.main()
+   
+if __name__=="__main__":
+    #unittest.main()
+    TestDataset("test_dataset").debug()
+    TestDataset("test_comparison").debug()
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -4,98 +4,51 @@ left snapshot, featurized_snapshot_2025-11-25.csv
 
 main features:
     * 
-
-
-
-    $ python -m unittest test.py
+    $ 
 """
 
 import unittest
 import importlib.resources as resources
 from pathlib import Path
 
-import pandas as pd
-
 from hsdu.dataset import D2TableDataset
-from hsdu.comparison import StanevSuperCon
+from hsdu.convert.utils import Converter 
+from hsdu.convert.feature import  MultiSourceFeaturizer
 
 class Test(unittest.TestCase):
     """Test core class methods"""
 
-    def test_dataset(self):
+    def test_convert(self):
         """in-house dataset test
         
-        load test version of in-house dataset
+        load the in-house dataset
         """
-        # load dataset
-        with resources.as_file(resources.files("hsdu.data.tests") /"test_dataset.csv") as path:
-            test_dataset_path = path
-        
-        # generate cleaned datatable
-        from hsdu.convert.utils import Converter 
+        test_dataset_path: Path=None
+        with resources.as_file(resources.files("hsdu.data.tests") /"full_dataset.csv") as path:
+            test_dataset_path = path if Path(path).is_file() else None
+
+        if test_dataset_path is None:
+            with resources.as_file(resources.files("hsdu.data.tests") /"test_dataset.csv") as path:
+                test_dataset_path = path
+
+        # generate cleaned datatable with comositional columns only
         converter = Converter(test_dataset_path, "compositional5_merge2maxTc_wholedataset.json")
         converter.convert(make_dir=True, exist_ok=True)
 
         # featurize from cleand datatable
-        from hsdu.dataset import D2TableDataset
-        from hsdu.convert.feature import  MultiSourceFeaturizer
         dataset = D2TableDataset(converter.save_compositional5_pth, exception_col=None)
         dataset.pymatgen_comps()
 
         featurizer=MultiSourceFeaturizer(config="xu.json")
         featurized_df = featurizer.featurize_all(dataset.df, merge_both=True, save_file="test_featurized_table.csv")
+        print(featurized_df)
 
         # delete temporal files
-        converter.save_compositional5_pth.unlink()
-        converter.save_log_pth.unlink()
-        Path("test_featurized_table.csv").unlink()
+        #converter.save_compositional5_pth.unlink()
+        #converter.save_log_pth.unlink()
+        #Path("test_featurized_table.csv").unlink()
         
 
     
 if __name__ == '__main__':
-    #Test().test_dataset() #it is convenience for debug..
-    #unittest.main()
-        
-    # old test codes; to be refactored
-    TEMP_TEST_FEATURIZE=True
-    TEMP_TEST_COMPARISON_TABLES=False
-
-    
-    if TEMP_TEST_FEATURIZE:
-        from pathlib import Path
-
-        import pandas as pd
-
-        from hsdu.dataset import Dataset, D2TableDataset
-
-        # load dataset
-        merged_dataset_path=Path(r"C:\Users\chyi\hsd_utils\temp_devs\hesc_dataset_16dec2025 - DataTable.csv")
-
-        # generate cleaned datatable
-        from hsdu.convert.utils import Converter 
-        converter = Converter(merged_dataset_path, "compositional5_merge2maxTc_wholedataset.json")
-        converter.convert(make_dir=True, exist_ok=True)
-
-        # featurize from cleand datatable
-        from hsdu.dataset import D2TableDataset
-        from hsdu.convert.feature import  MultiSourceFeaturizer
-        dataset = D2TableDataset(converter.save_compositional5_pth, exception_col=None)
-        dataset.pymatgen_comps()
-
-        featurizer=MultiSourceFeaturizer(config="xu.json")
-        featurized_df = featurizer.featurize_all(dataset.df, merge_both=True, save_file="featurized_table_16dec2025.csv")
-    if TEMP_TEST_COMPARISON_TABLES:
-        from hsdu.comparison import XuTestHEA, StanevSuperCon
-        from hsdu.convert.utils import Converter 
-        ss_dataset = StanevSuperCon(config="ss.json")
-        converter = Converter(ss_dataset, "comp5_ss.json")
-        converter.convert(make_dir=True, exist_ok=True, simple_target=True)
-
-        #%%
-        from hsdu.dataset import D2TableDataset
-        from hsdu.convert.feature import  MultiSourceFeaturizer
-        dataset = D2TableDataset(converter.save_compositional5_pth, exception_col=None)
-        dataset.pymatgen_comps()
-
-        featurizer=MultiSourceFeaturizer(config="xu.json")
-        featurized_df = featurizer.featurize_all(dataset.df, merge_both=True, save_file="test_featurized_table.csv")
+    Test("test_convert").debug()
