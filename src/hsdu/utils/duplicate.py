52bc39ede997f80f381092035ae3726de053abbc
changhyun yi <chhyyi@gmail.com>
2026-02-05 00:10:26 +0900

    working on duplicates groups validation..

--- src/hsdu/utils/duplicate.py
+++ src/hsdu/utils/duplicate.py
@@ -1,5 +1,6 @@
 #%%
 import argparse
+from typing import Literal
 
 import pandas as pd
 from scipy.spatial.distance import cdist
@@ -9,10 +10,11 @@ from pymatgen.core.periodic_table import Element
 from hsdu.dataset import Dataset
 from hsdu.utils.conversion_utils import element_list_iupac_ordered, OneHotFracCodec
 
-def distance_matrix(comps:list[Composition] | list[float] |list[list[float]],
-                    metric:str, elemental_set:list[str]|None = None,
+def distance_matrix(comps0:list[Composition] |list[list[float]],
+                    comps1:list[Composition] |list[list[float]],
+                    metric:Literal["chebyshev", "cityblock", "euclidean", "L1", "l1", "L2","l2", "L_infty", "maximum", "tchebychev", "l_infty"], elemental_set:list[str]|None = None,
                     exclude_expanded_elements:bool=True):
-    """simple wrapper of cdist from scipy.spatial.distance
+    """wrapper of cdist from scipy.spatial.distance
     
     Args:
         - comps: list[Composition] - list of pymatgen Compositions
@@ -22,33 +24,33 @@ def distance_matrix(comps:list[Composition] | list[float] |list[list[float]],
             - "l2": to "euclidean" 
                 - L2, Euclidean distance. $\sqrt{\sum_i{(x_i-y_i})^2}$
             - "l_infty" | "maximum" | "tchebychev": to "chebyshev"
-                - \max_i(|x_i-y_i|)
-            - any other valid metric for cdist(metric).
+                - \\max_i(|x_i-y_i|)
         - exclude_expanded_elements:
             if True, ignore elements atomic_number>103. default True.
     """
     assert exclude_expanded_elements
     max_atomic_number=103
+    comps_list=[]
+    for comps in (comps0, comps1):
+        if all(isinstance(comp, Composition) for comp in comps):
+            if elemental_set is None:
+                # load all elements symbols from pymatgen
+                # instead of building a new elemental set
+                elemental_set = element_list_iupac_ordered([i.symbol for i in Element][:max_atomic_number])
+            onehot_frac_codec = OneHotFracCodec(elemental_set)
+            comps = [onehot_frac_codec.encode(comp) for comp in comps]
+            comps_list.append(comps)
 
-    if all(isinstance(comp, Composition) for comp in comps):
-        if elemental_set is None:
-            # load all elements symbols from pymatgen
-            # instead of building a new elemental set
-            elemental_set = element_list_iupac_ordered([i.symbol for i in Element][:max_atomic_number])
-        onehot_frac_codec = OneHotFracCodec(elemental_set)
-        comps = [onehot_frac_codec.encode(comp) for comp in comps]
-    elif all(isinstance(frac, float) for frac in comps):
-        comps=[comps]
-    elif all(all(isinstance(frac, float) for frac in frac_row) for frac_row in comps):
-        pass
-    else:
-        raise TypeError(comps)
+        elif all(all(isinstance(frac, float) for frac in frac_row) for frac_row in comps):
+            comps_list.append(comps)
+        else:
+            raise TypeError(comps)
         
     alias_map = {al: "chebyshev" for al in ("L_infty", "maximum", "tchebychev", "l_infty")}
     alias_map = alias_map | {al: "cityblock" for al in ("l1", "L1")}
-    alias_map = alias_map | {al: "euclidean" for al in ("l2", "L1")}
-    metric = alias_map.get(metric, metric)
-    return cdist(comps, comps, metric=metric)
+    alias_map = alias_map | {al: "euclidean" for al in ("l2", "L2")}
+    metric_out: Literal["chebyshev","cityblock","euclidean"]= alias_map.get(metric, metric)
+    return cdist(comps_list[0], comps_list[1], metric=metric_out)
 
 def comps_pymatgen2elemental_set(pymatgen_comps: Composition):
     """make elements_set list"""
@@ -84,7 +86,9 @@ def merge_overlap(index, duplicates_group, group_rows):
             assert sum([1 if i in v else 0 for v in duplicates_group.values()])==1
         return duplicates_group, group_rows
         
-def make_duplicates_group(index, d1_dist_matrix, dinfty_dist_matrix, elements_sets, linfty_cutoff=0.01, l1_cutoff=0.02, cross_elements_set=False):
+def make_duplicates_group(index, d1_dist_matrix, dinfty_dist_matrix, elements_sets, linfty_cutoff=0.01, l1_cutoff=0.02, cross_elements_set=False, verbose=True):
+    if verbose:
+        print(f"start to make duplicates group; linfty_cutoff={linfty_cutoff}, l1_cutoff={l1_cutoff}")
     assert index.tolist()==list(range(len(elements_sets)))
     last_dup_group_idx = -1 # so first is 0
     current_group_idx = 0
